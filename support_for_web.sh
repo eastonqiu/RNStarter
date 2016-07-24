2dbaa2e8ed233607653f923a0f30e143f07b725a
qqm <qiuqingmo@lingyunstrong.com>
2016-07-24 16:45:57 +0800

    fix icon initial state to support web for material ui lib

---
+++ support_for_web.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ./modified/create-icon-set.js ./node_modules/react-native-vector-icons/lib/create-icon-set.js
+cp ./modified/IconToggle.js node_modules/react-native-material-design/lib/IconToggle.js
