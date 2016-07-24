b732bfb6387233ad186b21a6a76da7d8670abfd2
easton <eastonqiu@163.com>
2016-07-24 21:10:47 +0800

    fix color issue in Avatar that fixed by github master branch

--- support_for_web.sh
+++ support_for_web.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp ./modified/create-icon-set.js ./node_modules/react-native-vector-icons/lib/create-icon-set.js
 cp ./modified/IconToggle.js node_modules/react-native-material-design/lib/IconToggle.js
+cp ./modified/Avatar.js node_modules/react-native-material-design/lib/Avatar.js
