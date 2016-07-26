145a7fd81ca78311e53e05db8bcea1fd0a094b10
qqm <qiuqingmo@lingyunstrong.com>
2016-07-26 16:19:13 +0800

    fix splash bug

--- support_for_web.sh
+++ support_for_web.sh
@@ -2,3 +2,4 @@
 cp ./modified/create-icon-set.js ./node_modules/react-native-vector-icons/lib/create-icon-set.js
 cp ./modified/IconToggle.js node_modules/react-native-material-design/lib/IconToggle.js
 cp ./modified/Avatar.js node_modules/react-native-material-design/lib/Avatar.js
+cp ./modified/SplashScreen.java node_modules/rn-splash-screen/android/src/main/java/com/mehcode/reactnative/splashscreen/SplashScreen.java
